870ce61ca283a1aca037d4769aaca687b7677b7a
Yehor Potasieiev <potasey@gmail.com>
2023-06-23 16:16:38 +0300

    change all

--- specs/second.spec.js
+++ specs/second.spec.js
@@ -1,24 +1,40 @@
-const expect = require ("chai").expect
-const EC = protractor.ExpectedConditions
-browser.waitForAngularEnabled(false)
-describe ("Second test in my life", function(){
-    it ("second test", async function(){
-       
-       const ButtonStartGameSelector = "#root > div.Root--nY2ot > div.Root__Wrapper--IoJfb > div > div > div.InitialScreen__Container--RUB2M > div.InitialScreen__StartMenu--qhHCS > button"
-       const ButtonStart = $(ButtonStartGameSelector)
-       const ButtonSecondStartSelector = "#root > div.Root--nY2ot > div.Root__Wrapper--IoJfb > div > div > form > button"
-       const ButtonSecondStart = $(ButtonSecondStartSelector)
-       
-       
-       
-        await browser.get("https://app.metafora.game/#/")
-        await browser.wait(EC.visibilityOf(ButtonStart), 7000, "Sorry no button no memes")
-        await browser.wait(EC.visibilityOf(ButtonSecondStart), 7000, "Sorry no button no names")
-       
-       
-        await ButtonStart.click()
-      
-        await ButtonSecondStart.click()
-        await browser.sleep (10050)
-    })
-})
+const expect = require("chai").expect;
+const EC = protractor.ExpectedConditions;
+browser.waitForAngularEnabled(false);
+
+describe("Second test in my life", function () {
+  it("second test", async function () {
+    const ButtonStartGameSelector = "gameCreateButton";
+    const ButtonStart = $(ButtonStartGameSelector);
+    const ButtonSecondStartSelector = "#playerFormSubmitButton";
+    const ButtonSecondStart = $(ButtonSecondStartSelector);
+    const ButtonGameSettingsSelector = "#gameSettingsSubmitButton";
+    const ButtonGameSettings = $(ButtonGameSettingsSelector);
+
+    await browser.get("https://test3-metafora-game.web.app");
+
+    await browser.wait(
+      EC.visibilityOf(ButtonStart),
+      15*1000,
+      "Sorry no button ButtonStart"
+    );
+    await browser.wait(
+      EC.visibilityOf(ButtonSecondStart),
+      7000,
+      "Sorry no button ButtonSecondStart"
+    );
+    await browser.wait(
+      EC.visibilityOf(ButtonGameSettings),
+      7000,
+      "Sorry no button ButtonGameSettings"
+    );
+
+    await ButtonStart.click();
+
+    await ButtonSecondStart.click();
+
+    await ButtonGameSettings.click();
+
+    await browser.sleep(10050);
+  });
+});
